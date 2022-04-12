89f04544c6a068aaa415e9d4b2a9a1ae3b2920c8
Abdul Basit Khan <99525295+AbdulBasit2733@users.noreply.github.com>
2022-04-12 12:38:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+# import random
+# from menu import MenuItem,Menu
+# from money_machine import MoneyMachine
+# from coffee_maker import CoffeeMaker
+# menu = Menu()
+# coffee_maker = CoffeeMaker()
+# money_machine = MoneyMachine()
+#
+# is_on = True
+#
+#
+# while is_on:
+#     options = menu.get_items()
+#     choice = input(f"What Would You Like ? ({options}) :").lower()
+#     if choice == "off":
+#         is_on = False
+#     elif choice == "report":
+#         coffee_maker.report()
+#         money_machine.report()
+#     else:
+#         drink = menu.find_drink(choice)
+#         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+#             coffee_maker.make_coffee(drink)
+
+from menu import MenuItem, Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+menu = Menu()
+options = menu.get_items()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+while is_on:
+    choice = input(f"What Would You Like? {options} : ").lower()
+    if choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == "off":
+        is_on = False
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
